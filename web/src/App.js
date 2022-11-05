49a951281b58d859e39f19da8400a260ed938fe3
Naman Dwivedi <namandwivedi14@gmail.com>
2022-11-05 02:38:03 -0700

    wallet connect setup

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,107 @@
+import ReactDOM from "react-dom";
+
 import logo from './logo.svg';
 import './App.css';
+import { BigNumber, ethers } from 'ethers'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      connectedAccount: undefined
+    }
+  }
+
+  componentDidMount() {
+    this.checkIfWalletIsConnected()
+  }
+
+
+  checkIfWalletIsConnected = async () => {
+
+    this.setState({ loading: true })
+
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!")
+      this.setState({ loading: false })
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+      console.log(window.ethereum.networkVersion, 'window.ethereum.networkVersion');
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      this.onWalletConnected(account)
+      this.setState({ loading: false })
+    } else {
+      console.log("No authorized account found")
+      this.setState({ loading: false })
+    }
+
+    ethereum.on('accountsChanged', (accounts) => {
+      const account = accounts[0];
+      this.onWalletConnected(account)
+    });
+  }
+
+  onWalletConnected = async (account) => {
+    if (account) {
+      console.log("Found an authorized account:", account);
+      this.setState({ connectedAccount: account })
+    }
+  }
+
+  connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+
+      }
+      if (this.state.connectedAccount == '') {
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        this.onWalletConnected(accounts[0])
+      } else {
+        const permissions = await ethereum.request({ method: "wallet_requestPermissions", params: [{ eth_accounts: {} }] });
+        const accountsPermission = permissions.find(
+          (permission) => permission.parentCapability === 'eth_accounts'
+        );
+        if (accountsPermission) {
+          const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+          this.onWalletConnected(accounts[0])
+        }
+      }
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  renderUI = () => {
+
+  }
+
+  render() {
+    return (
+      <div className="container">
+
+        {!this.state.connectedAccount && <button type="button" onclick="connectWallet()">Connect wallet</button>}
+         
+        {this.state.loading && <div className="loader"/>}
+
+      </div>
   );
+  }
 }
-
-export default App;
