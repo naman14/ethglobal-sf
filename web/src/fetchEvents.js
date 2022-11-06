2bcde76e34051afac5455dfac16c9cc1f025a8ca
Naman Dwivedi <namandwivedi14@gmail.com>
2022-11-06 00:18:20 -0700

    fix events fetching

--- web/src/fetchEvents.js
+++ web/src/fetchEvents.js
@@ -13,32 +13,29 @@ export const fetchEvents = async () => {
         },
         "referrer": "https://thegraph.com/",
         "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": "{\"query\":\"{\\n  events(first: 10) {\\n    id\\n    eventTitle\\n    eventDescription\\n    price\\n    organiser\\n    organiserContact\\n    coverImageUri\\n    totalTickets\\n    startTime\\n    endTime\\n    createdAt\\n  }\\n}\",\"variables\":null}",
+        "body": "{\"query\":\"{\\n  events(first: 10) {\\n    id\\n    eventTitle\\n   location\\n    eventDescription\\n    price\\n    organiser\\n    organiserContact\\n    coverImageUri\\n    totalTickets\\n    startTime\\n    endTime\\n    createdAt\\n  }\\n}\",\"variables\":null}",
         "method": "POST",
         "mode": "cors",
         "credentials": "omit"
     });
 
     let responseJson = await response.json()
-    console.log(responseJson)
+    let events = responseJson.data.events
 
-    return responseJson.data.events
-
-    // // console.log(events)
-    // return events.map((event) => {
-    //     return {
-    //         lockAddress: event.lockAddress,
-    //         eventTitle: event.eventTitle,
-    //         eventDescription: event.eventDescription,
-    //         price: event.price.toString(),
-    //         location: event.location,
-    //         organiser: event.organiser,
-    //         organiserContact: event.organiserContact,
-    //         coverImageUri: event.coverImageUri,
-    //         totalTickets: event.totalTickets.toString(),
-    //         startTime: event.startTime.toString(),
-    //         endTime: event.endTime.toString(),
-    //         createdAt: event.createdAt.toString()
-    //     }
-    // })
+    return events.map((event) => {
+        return {
+            lockAddress: event.id,
+            eventTitle: event.eventTitle,
+            eventDescription: event.eventDescription,
+            price: event.price.toString(),
+            location: event.location,
+            organiser: event.organiser,
+            organiserContact: event.organiserContact,
+            coverImageUri: event.coverImageUri,
+            totalTickets: event.totalTickets.toString(),
+            startTime: event.startTime.toString(),
+            endTime: event.endTime.toString(),
+            createdAt: event.createdAt.toString()
+        }
+    })
 }
