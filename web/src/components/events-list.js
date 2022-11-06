76215475dc2ec9c2c8d9e5c876e089855f7216c4
Mohammad Umair <umairmohd@fb.com>
2022-11-06 01:40:53 -0700

    dynamic attendee count
    Signed-off-by: Mohammad Umair <umairmohd@fb.com>

--- web/src/components/events-list.js
+++ web/src/components/events-list.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react'
 
 import './events-list.css';
-import { fetchEvents } from '../fetchEvents';
+import { fetchEvents, fetchPeopleCounts } from '../fetchEvents';
 import EventListItem from './event-list-item';
 
 export default class EventList extends Component {
     constructor(props) {
         super()
         this.state = {
-            events :[]
+            events :[],
+            attendeeCount: {}
         }
     }
     componentDidMount() {
@@ -16,15 +17,23 @@ export default class EventList extends Component {
     }
     getEvents = async () => {
         const e = await fetchEvents()
-        console.log(e)
-        this.setState({events: e})
+        // console.log(e)
+        let lockAddresses = []
+        for (let i = 0; i < e.length; ++i) {
+            lockAddresses.push("\"" + e[i].lockAddress + "\"")
+        }
+        const ac = await fetchPeopleCounts(lockAddresses)
+        // console.log(ac)
+        this.setState({events: e, attendeeCount: ac})
     }
     render() {
         return (
             <div style={{paddingLeft: '200px', paddingRight: '200px', backgroundColor: 'black'}}>
                 <div id="event-list-bg">
                     {this.state.events.map((event, index) => {
-                    return <EventListItem event={event} key={index}></EventListItem>
+                        // console.log("derp")
+                        // console.log(event)
+                    return <EventListItem event={event} attendeeCount={this.state.attendeeCount.get(event.lockAddress)} key={index}></EventListItem>
                 })}
                 </div>
             </div>
