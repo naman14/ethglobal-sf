1d0781ce91d663a5656eb4fb803547521bdb3524
Raghav Apoorv <raghav.apoorv@uber.com>
2022-11-05 20:23:28 -0700

    Integrate create event contract

--- web/src/createEvent.js
+++ web/src/createEvent.js
@@ -1,19 +1,19 @@
-import { ethers, BigNumber } from "ethers"
+import {ethers, BigNumber} from "ethers"
 
-import { signedEventContract } from "./constants"
+import {signedEventContract} from "./constants"
 
-    // address lockAddress; // keep 0x0 
-    // string eventTitle;
-    // string eventDescription;
-    // uint256 price;
-    // string location;
-    // string organiser;
-    // string organiserContact;
-    // string coverImageUri;
-    // uint256 totalTickets;
-    // uint256 startTime;
-    // uint256 endTime;
-    // uint256 createdAt;
+// address lockAddress; // keep 0x0
+// string eventTitle;
+// string eventDescription;
+// uint256 price;
+// string location;
+// string organiser;
+// string organiserContact;
+// string coverImageUri;
+// uint256 totalTickets;
+// uint256 startTime;
+// uint256 endTime;
+// uint256 createdAt;
 
 // let eventDetails = [
 //     '0x0000000000000000000000000000000000000000',
@@ -30,9 +30,11 @@ import { signedEventContract } from "./constants"
 //     Date.now()
 // ]
 
-export const createNewEvent  = async (eventDetails) => {
+export const createNewEvent = async (eventDetails) => {
     console.log('creating new event')
-    let contract = signedEventContract()
+    eventDetails.address = '0x0000000000000000000000000000000000000000';
+    eventDetails.createdAt = Date.now();
+    let contract = signedEventContract();
     console.log(contract)
     let tx = await contract.createEvent(eventDetails, {value: '0'})
     console.log(tx)
